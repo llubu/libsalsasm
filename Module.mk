f99ceccd22e5a3b5e5f3dfa084baab197690d145
Ryan Salsamendi <rsalsamendi@paloaltonetworks.com>
2014-08-10 12:39:30 -0700

    Ensure functions tagged for export get exported!

--- Module.mk
+++ Module.mk
@@ -1,4 +1,6 @@
 libsalsasm_HEADERS := salsasm.h decode.h codegen.h salsasm_types.h
 libsalsasm_SOURCES := salsasm.c decode.c
 
+libsalsasm_CFLAGS += -DLIBSALSASM_EXPORTS
+
 $(eval $(call CREATE_MODULE,libsalsasm,LIB))
